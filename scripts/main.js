ef49deed4c74984753eb3be8c346590da250ea77
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-02-12 08:58:00 +0100

    implemented the 'Create' functionality of CRUD

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-console.log("Chibuike");
+var textInput = document.getElementById("text-input");
+var todosContainer = document.getElementById("todos-container");
+var todoCount = 0;
+textInput.onkeydown = function (e) {
+    if (e.key === "Enter") {
+        todoCount++;
+        console.log(textInput.value);
+        var todoContainer = document.createElement("div");
+        todoContainer.className = "todo-container";
+        var todoItem = document.createElement("div");
+        todoItem.className = "todo-item";
+        todoContainer.appendChild(todoItem);
+        todosContainer === null || todosContainer === void 0 ? void 0 : todosContainer.appendChild(todoContainer);
+        var uniqueId = "task-".concat(todoCount);
+        var checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.className = "checkbox";
+        checkBox.id = uniqueId;
+        todoItem.appendChild(checkBox);
+        var label = document.createElement("label");
+        label.setAttribute("for", uniqueId);
+        label.textContent = textInput.value;
+        todoItem.appendChild(label);
+    }
+};
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,2 +1,32 @@
-const textInput = document.getElementById(".text-input");
+const textInput = document.getElementById("text-input") as HTMLInputElement;
 const todosContainer = document.getElementById("todos-container");
+
+let todoCount = 0;
+
+textInput.onkeydown = (e) => {
+	if (e.key === "Enter") {
+		todoCount++;
+
+		console.log(textInput.value);
+		const todoContainer = document.createElement("div");
+		todoContainer.className = "todo-container";
+		const todoItem = document.createElement("div");
+		todoItem.className = "todo-item";
+		todoContainer.appendChild(todoItem);
+		todosContainer?.appendChild(todoContainer);
+
+		const uniqueId = `task-${todoCount}`;
+
+		const checkBox = document.createElement("input");
+		checkBox.type = "checkbox";
+		checkBox.className = "checkbox";
+		checkBox.id = uniqueId;
+
+		todoItem.appendChild(checkBox);
+
+		const label = document.createElement("label");
+		label.setAttribute("for", uniqueId);
+		label.textContent = textInput.value;
+		todoItem.appendChild(label);
+	}
+};
